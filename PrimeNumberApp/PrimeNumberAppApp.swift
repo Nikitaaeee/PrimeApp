a0965fa40236b7c43993caa0614254f9e24adb4a
Nikita Kirshin <Nik.a.kirshin@gmail.com>
2023-03-29 13:50:51 +0300

    Initial Commit

---
+++ PrimeNumberApp/PrimeNumberAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrimeNumberAppApp.swift
+//  PrimeNumberApp
+//
+//  Created by Nikita Kirshin on 29.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct PrimeNumberAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
